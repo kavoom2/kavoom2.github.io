fbd799cdcd2cdc3dc7b7a00608d7ae95f88cc341
kavoom2 <kavoom2@gmail.com>
2021-01-04 17:29:03 +0900

    changed theme

---
+++ _posts/2021-01-01-arguments.md
@@ -0,0 +1,99 @@
+---
+title: "Arguments와 Rest parameter"
+classes: wide
+
+categories:
+  - Javascript101
+---
+
+
+## 1. arguments 
+arguments는 함수에 전달한 인자들을 참조하는 객체입니다. 
+
+````
+function foo(a, b, c) {
+  console.log(arguments[0]); // 1
+  console.log(arguments[1]); // 2
+  console.log(arguments[2]); // 3
+};
+foo(1, 2, 3);;
+````
+
+arguments은 유사배열입니다. 배열처럼 인덱스는 0에서부터 시작하며, 각 인수를 설정하거나 재할당할 수 있습니다.
+
+> arguments[0]; // arguments의 0번째 인덱스    
+> arguments[1]; // arguments의 1번째 인덱스    
+> arguments[2] = 'new value'; // arguments의 2번째 인덱스 값을 'new value'로 설정 또는 재할당
+
+arguments 객체는 Array가 아닙니다. 배열에서 사용할 수 있는 pop(), shift() 등 프로퍼티를 가지고 있지 않습니다. 대신, 다음과 같은 방법으로 arguments를 배열로 변환할 수 있습니다.
+
+> let arg = Array.prototype.slice.call(arguments);    
+> let args = Array.from(arguments);    
+> let args = [...arguments];
+
+## 1.1. 예제
+arguments 객체는 다음과 같이 전달인자를 변수에 할당하기 힘든 상황에서 유용합니다.
+````
+let memoize = function (func) {
+  let cache = {};
+  let result;
+
+  return function() {
+    let args = Array.prototype.slice.call(arguments);
+    if ( args in cache ) {
+    return cache[args];
+    }
+    else {
+    cache[args] = func.apply.(null, args);
+    };
+  };
+};
+
+const add = memoize(function(a,b) {
+  return a + b;
+});
+
+add(2, 3); // 5
+add(5, 8); // 13
+````
+
+
+함수 memoize는 어떤 함수에 이전에 전달받은 인자들을 다시 받으면, 즉시 함수 내에 저장한 결과값을 출력합니다. 어떤 인자를 입력했을 때, 그 인자가 한 번 이상 입력받았는지 확인해야 합니다. 즉, 현재 받은 인자가 캐시 안에 있는지 여부를 판단해야 합니다.
+
+변수 add는 memoize가 반환한 함수 function() {.....}이 할당되어 있습니다. add(2, 3)을 입력하면, arguments 객체는 전달인자 2, 3을 참조합니다.
+
+## 2. Rest parameter
+Rest parameter는 정해지지 않은 수의 인자를 배열로 나타냅니다. 
+
+````
+function foo(a, b, ...args) {
+  console.log('a', a);
+  console.log('b', b);
+  console.log('args', args);
+};
+
+foo('one', 'two', 'three', 'four', 'five', 'six');
+
+// a, one
+// b, two
+// args, [three, four, five, six]
+````
+
+함수의 마지막 매개변수에 **...**을 붙여 모든 나머지 인자들을 배열로 대체합니다. 위 예제는 a, b를 제외한 나머지 인자들을 args 배열로 대체합니다.
+
+arguments 객체와 달리 Rest parameter는 실제 배열이며, 배열의 모든 속성에 접근할 수 있습니다. Array의 인스턴스이므로 sort, map, forEach, pop 등 메소드를 바로 적용할 수 있습니다.
+
+````
+// 위 예제와 동일한 함수를 사용
+
+foo('one', 'two');
+// a, one
+// b, two
+// args, []
+````
+
+한편, Rest parameter에 인자를 넣지 않으면 args은 빈배열이 됩니다.
+
+# Reference
+*https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/rest_parameters
+*https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/arguments
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,31 @@
-var store = []
+var store = [{
+        "title": "Arguments와 Rest parameter",
+        "excerpt":"1. arguments arguments는 함수에 전달한 인자들을 참조하는 객체입니다. function foo(a, b, c) { console.log(arguments[0]); // 1 console.log(arguments[1]); // 2 console.log(arguments[2]); // 3 }; foo(1, 2, 3);; arguments은 유사배열입니다. 배열처럼 인덱스는 0에서부터 시작하며, 각 인수를 설정하거나 재할당할 수 있습니다. arguments[0]; // arguments의 0번째 인덱스 arguments[1]; // arguments의 1번째 인덱스 arguments[2] =...","categories": ["Javascript101"],
+        "tags": [],
+        "url": "https://kavoom2.github.io/javascript101/arguments/",
+        "teaser": null
+      },{
+        "title": "클로져(Closure)",
+        "excerpt":"1. 클로저(Closure)란 클로저(closure)는 함수와 그 함수가 선언되었을 때 렉시컬 환경(lexical environment)과의 조합입니다. 스코프(scope)는 함수를 호출할 때가 아니라 함수를 어디에 선언하였는지에 따라 결정됩니다. 이러한 개념을 렉시컬 스코핑(Lexical Scoping)라 합니다. function outer() { let x = 10; let inner = function() { console.log(x); }; return inner; }; outer(); // 10 위 예제에서...","categories": ["Javascript101"],
+        "tags": [],
+        "url": "https://kavoom2.github.io/javascript101/closure/",
+        "teaser": null
+      },{
+        "title": "복잡도(Complexity)",
+        "excerpt":"1. 복잡도 처리해야 할 데이터의 양이 방대해지면서, 알고리즘의 효율성 차이가 커질 수 밖에 없습니다. 가령, 데이터를 정렬하더라도 정렬 알고리즘마다 정렬에 걸리는 시간은 천차만별입니다. 방대한 데이터를 더 빨리 처리할 수 있는 방법을 선택해야겠습니다. 서로 다른 알고리즘을 비교할 때, 하드웨어와 소프트웨어 환경이 천차만별이라면 결과를 신뢰하기 힘들겁니다. 계산복잡도는 이러한 환경을 배제하고 알고리즘이 얼마나...","categories": ["Javascript101"],
+        "tags": [],
+        "url": "https://kavoom2.github.io/javascript101/complexity/",
+        "teaser": null
+      },{
+        "title": "재귀(Recursion)",
+        "excerpt":"1. 재귀란? 컴퓨터 과학에서 재귀(recursion)은 자신을 정의할 때 자기 자신을 참조하는 것이라고 합니다. 프로그래밍에서 재귀함수는 자기 자신을 호출하도록 정의한 함수입니다. 1. 1. 재귀함수의 구성요소 재귀함수는 베이스(base case)와 재귀단계(recursive case)로 구성됩니다. x를 n 제곱하는 함수 pow(x, n)을 예시로 재귀함수의 구성과 작동원리를 이해해봅시다. function pow(x, n) { if (n === 1) {...","categories": ["Javascript101"],
+        "tags": [],
+        "url": "https://kavoom2.github.io/javascript101/recursion/",
+        "teaser": null
+      },{
+        "title": "This",
+        "excerpt":"1. this란 this는 모든 함수 스코프(scope) 내에서 자동으로 설정되는 식별자입니다. 실행 컨텍스트(Execution Context)의 구성요소로 함수가 실행되는 동안 이용할 수 있습니다. 어느 지점에서 this를 호출하였는지에 따라 컨텍스트가 달라집니다. 2. 바인딩 규칙 규칙은 다음과 같이 5가지로 구분할 수 있다. 구분 호출방식 this 기본 바인딩 global window(전역객체) 기본 바인딩 function 호출 window(전역객체) 암시적...","categories": ["Javascript101"],
+        "tags": [],
+        "url": "https://kavoom2.github.io/javascript101/this/",
+        "teaser": null
+      }]
